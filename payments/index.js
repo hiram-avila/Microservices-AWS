6d1bdb89e333882316346a6c5f42a5b44803b4e4
Hiram <Hiramavilaa@gmail.com>
2024-08-13 18:00:14 -0600

    test final

--- payments/index.js
+++ payments/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Ruta de ejemplo
+
 app.get('/payments', (req, res) => {
     res.send('Payments');
 });
 
-// Iniciar el servidor
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
 });
-q
