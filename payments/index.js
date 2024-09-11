bce6de25c8e0a797679ee7c88fbb8f74a2a68343
Hiram <Hiramavilaa@gmail.com>
2024-09-10 22:08:27 -0600

    correction mistakes

--- payments/index.js
+++ payments/index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
+app.get('/payments', (req, res) => {
+    res.send('Payments aqui escuchando');
+});
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
